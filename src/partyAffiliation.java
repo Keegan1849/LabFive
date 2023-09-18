93e6037031a064a93167740e0224febc685fe38d
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-18 11:48:46 -0400

    second push

--- src/partyAffiliation.java
+++ src/partyAffiliation.java
@@ -10,16 +10,19 @@ public class partyAffiliation {
         String oth = "O";
         System.out.println("What is your party affiliation? [R,D,I,O]");
         String line = in.nextLine();
-        if (line.equals("R")) {
-            System.out.println("You are a Republican Elephant! ");
-        } else if (line.equals("D")) {
-            System.out.println("You are a Democratic Donkey! ");
-        }
-		else if (line.equals("I")) {
-            System.out.println("You are a Independent Man! ");
-        }
-        else if (line.equals("O")) {
-            System.out.println("You are a other party");
+        switch (line) {
+            case "R":
+                System.out.println("You are a Republican Elephant! ");
+                break;
+            case "D":
+                System.out.println("You are a Democratic Donkey! ");
+                break;
+            case "I":
+                System.out.println("You are a Independent Man! ");
+                break;
+            case "O":
+                System.out.println("You are a other party");
+                break;
         }
 
     }
