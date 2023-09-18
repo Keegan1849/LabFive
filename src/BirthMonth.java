5a5b7a05f075be68b2278a722ea6d31896152ffb
[Keegan Chapin] <[keeg.chapin@gmail.com]>
2023-09-18 19:29:21 -0400

    for real

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -5,7 +5,6 @@ public class BirthMonth {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
         System.out.println("What is your birth month? ");
         int birthMonth = Integer.parseInt(in.nextLine());
         if (birthMonth > 12){
@@ -13,7 +12,7 @@ public class BirthMonth {
         } else if (birthMonth  < 1) {
             System.out.println("That is an invalid age: " + birthMonth);
         } else {
-            System.out.println("That is an valid age: " + birthMonth);
+            System.out.println("Your birth month is: " + birthMonth);
 
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
         System.out.println("What is the price of your item? ");
         price = in.nextDouble();
-        if (price >= 100){
+        if (price < 100){
             shipping = price * .02;
             totalPrice = price + shipping;
             System.out.println("The cost of shipping is " + shipping + " and the cost of your item is " + totalPrice);
-        } else if(price < 100){
+        } else if(price >= 100){
             System.out.println("The cost of your item is " + price);
 
         }
